b05a5b91c4e3b0f44c74572952eeaaafa2b09d65
SterlingTian1 <sterling@smallneighborhood.com>
2020-11-25 15:38:43 -0600

    updated resume

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -29,7 +29,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-      {/* <ParticlesBg type="circle" bg={true} /> */}
+      <ParticlesBg type="circle" bg={true} />
      
       <nav id="nav-wrap">
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
@@ -56,7 +56,7 @@ class Header extends Component {
             </ul>
             
          </div>
-         <ImageGallery items={images} />
+         {/* <ImageGallery items={images} /> */}
       </div>
 
       <p className="scrolldown">
