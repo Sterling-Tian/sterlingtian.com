9dc095ded0ac607a37b4e541fcd2e3e0a7b82afd
SterlingTian1 <sterling@smallneighborhood.com>
2021-01-08 19:52:40 -0600

    adds testimonials

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import About from './Components/About';
 import Resume from './Components/Resume';
 import Contact from './Components/Contact';
 import Portfolio from './Components/Portfolio';
+import Testimonials from './Components/Testimonials';
 
 class App extends Component {
 
@@ -49,7 +50,7 @@ class App extends Component {
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
         <Portfolio data={this.state.resumeData.portfolio}/>
-        <Contact data={this.state.resumeData.main}/>
+        <Testimonials data={this.state.resumeData.testimonials}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
     );
